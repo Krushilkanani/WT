3199c29fa27a999a095d6007e9a634da3b4ae5a1
Krushilkanani <96437007+Krushilkanani@users.noreply.github.com>
2023-02-26 13:50:25 +0530

    Add files via upload

---
+++ es6.js
@@ -0,0 +1,15 @@
+var myConcat = function(arr1, arr2) {
+    return arr1.concat(arr2);
+  };
+  
+  console.log(myConcat([1, 2], [3, 4, 5]));
+
+  const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+   
+  const today = HIGH_TEMPERATURES.today;
+  const tomorrow = HIGH_TEMPERATURES.tomorrow;
+  
